81c7fb709d188ef21e80814c19161b6e0bac8eba
Nicolene Marais <nici.marais.ie@gmail.com>
2023-03-02 09:18:38 +0000

    added FunctionGreetingWithProps - Follow Alaong Video

---
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionalGreeting(){
+//     return <h1>Hello From React!</h1>
+// }
+
+const FunctionalGreetingWithProps = () => <h1>Hello Props!</h1>
+
+export default FunctionalGreetingWithProps
