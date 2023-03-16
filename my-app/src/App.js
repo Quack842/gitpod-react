2721b169a0ef37331145abf473bf86a23fab1e1e
Nicolene Marais <nici.marais.ie@gmail.com>
2023-03-16 09:00:54 +0000

    commiting new files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,12 +13,13 @@ import './App.css';
 // import LifeCyclesCDU from './components/LifeCyclesCDU';
 // import LifeCyclesCWU from './components/LifeCyclesCWU';
 // import ControlledForm from './components/ControlledForm';
-import UncontrolledForm from './components/UncontrolledForm';
+// import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <UncontrolledForm />
+      <SearchBar />
     </div>
   );
 }
